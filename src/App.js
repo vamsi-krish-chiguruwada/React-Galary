ef75b9d1dc70f40cff70639ff9e0316faffc1618
Vamsi krish <vamsikrishchiguruwada@gmail.com>
2020-10-20 22:38:07 -0700

    day-1  19th oct 2020 added Navbar
    added Navbar and made it responsive,

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import './App.css';
+import Navbar from "./Components/Navbar/Navbar";
+import Searchbox from "./Components/Searchbox/Searchbox";
+function App() {
+  return (
+    <div>
+      <Navbar/>
+      <Searchbox/>
+      
+    </div>
+  );
+}
+
+export default App;
---
+++ src/Components/Searchbox/Searchbox.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './Searchbox.css';
+import imgBackground from "../../Images/hero.jpg";
+function Searchbox() {
+  return (
+    <div className = "search-wraper">
+        <img src = {imgBackground}></img>
+    </div>
+  );
+}
+
+export default Searchbox;
